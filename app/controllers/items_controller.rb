4999958164be02c9c0e8887e87593b9e433f3d8e
masaya <ohmiya_masaya@MacBook-Pro.local>
2020-06-22 14:58:18 +0900

    create controller user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,21 @@ class ItemsController < ApplicationController
     def index
         @items = Item.all
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        Item.create(name: item_params[:name], item_params[:price], user_id: current_user.id)
+    end
+
+    def move_to_index
+        redirect_to action: :index unless user_sighed_in?
+    end
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price)
+    end
 end
