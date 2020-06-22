3545d7f526fe2eb2273ef9af6bf27fa4ed4497f9
masaya <ohmiya_masaya@MacBook-Pro.local>
2020-06-22 16:33:30 +0900

    edit nickname

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,25 @@
 class ItemsController < ApplicationController
+    before_action :move_to_index, except: :index
+
     def index
-        @items = Item.all
+     @items = Item.all
     end
-
+  
     def new
-        @item = Item.new
+      @item = Item.new
     end
-
+  
     def create
-        Item.create(name: item_params[:name], item_params[:price], user_id: current_user.id)
+      Item.create(name: item_params[:name], price: item_params[:price], user_id: current_user.id)
     end
-
+  
     def move_to_index
-        redirect_to action: :index unless user_sighed_in?
+      redirect_to action: :index unless user_signed_in?
     end
+  
     private
-
+  
     def item_params
-        params.require(:item).permit(:name, :price)
+      params.require(:item).permit(:name, :price)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @items = User.find(user_id: current_user.id)
+        @user = User.find(current_user.id)
     end
 end
